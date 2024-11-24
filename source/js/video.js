83e807bd75448dce4f86a7dc7345b87a26e79bea
EugeneChepkasov <evg.chepkasov@gmail.com>
2024-11-25 02:40:48 +0300

    Верстка раздела о клубе

---
+++ source/js/video.js
@@ -0,0 +1,19 @@
+const videoContainer = document.querySelector('.video');
+const videoImg = document.querySelector('.video__media');
+const videoButton = document.querySelector('.video__button');
+
+const loadVideo = () => {
+  const iframe = document.createElement('iframe');
+  iframe.src = 'https://www.youtube.com/embed/9TZXsZItgdw?si=509Z4Bcb0db2ABVJ?rel=0&showinfo=0&autoplay=1';
+  iframe.classList.add('video__media');
+  iframe.setAttribute('allow', 'autoplay');
+  videoImg.remove();
+  videoButton.remove();
+  videoContainer.append(iframe);
+};
+
+const videoAboutHandler = () => {
+  videoButton.addEventListener('click', loadVideo);
+};
+
+export {videoAboutHandler};
